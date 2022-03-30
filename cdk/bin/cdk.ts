8d4db9ea1fa63975f2fe3e060fb64c856dc766ac
sebastianden <sebastian.dengler@protonmail.com>
2022-03-30 22:49:37 +0200

    Corrected name of test stack

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,4 +4,4 @@ import { CdkStack, TestStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
 new CdkStack(app, 'CdkStack');
-new TestStack(app, 'TestStak');
+new TestStack(app, 'TestStack');
