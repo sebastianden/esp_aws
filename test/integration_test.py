84a51768e1e77fb37b0344534942c7b020c92df2
sebastianden <sebastian.dengler@protonmail.com>
2022-04-02 11:20:14 +0200

    fixed lambda response

--- test/integration_test.py
+++ test/integration_test.py
@@ -2,9 +2,12 @@ import boto3
 
 client = boto3.client('lambda')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    try:
+        response = client.invoke(
+            FunctionName='iot-integration-test-lambda')
 
-    response = client.invoke(
-        FunctionName='iot-integration-test-lambda')
-
-    assert response['status'] == 200
+        assert response['ResponseMetadata']['HTTPStatusCode'] == 200
+        print('Successfully ran integration tests.')
+    except Exception:
+        print('Integration tests failed.')
